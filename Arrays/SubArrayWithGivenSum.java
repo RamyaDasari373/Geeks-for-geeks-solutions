9b9dc10bac4c8089d25415cf038d174e85898ffa
Ramya <107109350+RamyaDasari373@users.noreply.github.com>
2022-08-19 12:32:06 +0530

    Easy

--- Arrays/SubArrayWithGivenSum.java
+++ Arrays/SubArrayWithGivenSum.java
@@ -1,3 +1,4 @@
+//Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
 class Solution
 {
     //Function to find a continuous sub-array which adds up to a given number.
